91dd3d6eed489a2665aeb2bf1af9014ee09d7b20
William Ramos <contato.williamramos@outlook.com>
2021-03-03 01:23:58 -0300

    Implementando o método insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,42 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		SellerDao sd = DaoFactory.createSellerDao();
 		System.out.println("==== TEST 1: findById ====");
 		Seller sl = sd.findById(3);
 		System.out.println(sl);
+		
 		List<Seller> sellers = sd.findByDepartment(2);
-		System.out.println("==== TEST 2: findByDepartment ====");
+		System.out.println("\n==== TEST 2: findByDepartment ====");
 		for(Seller s : sellers) {
-			System.out.println(s + "/n");
+			System.out.println(s);
 		}
+		
 		List<Seller> sellersAll = sd.findAll();
-		System.out.println("==== TEST 3: findAll ====");
+		System.out.println("\n==== TEST 3: findAll ====");
 		for(Seller s : sellersAll) {
 			System.out.println(s + "/n");
 		}
+		
+		System.out.println("\n==== TEST 4: Insert ====");
+		
+		Seller sl2 = new Seller(null, "José Bezerra", "jose@gmail.com", new Date (sdf.parse("03/03/1985").getTime()), 4000.0, new Department(2, null));
+		sd.insert(sl2);
+		
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
@@ -16,6 +19,7 @@ import model.entities.Seller;
 public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public SellerDaoJDBC() {
 	}
@@ -26,8 +30,40 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller\r\n" + 
+					"(Name, Email, BirthDate, BaseSalary, DepartmentId)\r\n" + 
+					"VALUES\r\n" + 
+					"(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, 4000.0);
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! ID = " + id);
+				}
+			}else {
+				System.out.println("No rows affected");
+			}	
+		}		
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
@@ -73,6 +109,7 @@ public class SellerDaoJDBC implements SellerDao {
 
 	private Seller instanteateSeller(ResultSet rs, Department dep) throws SQLException{
 		Seller obj = new Seller();
+		obj.setName(rs.getString("Name"));
 		obj.setId(rs.getInt("Id"));
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
@@ -151,5 +188,4 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 	}
-	
 }
